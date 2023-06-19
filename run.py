5b1a2f648f962fac993902a08bb202fc97985493
Caroline Danielsson <caroline.e.danielsson@gmail.com>
2023-06-19 05:30:31 +0000

    Create display_contact_menu function for user menu selection in Contact Book

--- run.py
+++ run.py
@@ -12,7 +12,17 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('contact_book')      
 
-contact = SHEET.worksheet('contact')
-data = contact.get_all_values()
+def display_contact_menu():
+    """
+    Displays a welcome message and the menu for the Contact Book.
+    """
+    print("\nWelcome to Contact Book!\n")
+    print("1. Add a new contact")
+    print("2. Search for a contact")
+    print("3. Display all contacts")
+    print("4. Delete a contact")
+    print("5. Exit\n")
 
-print(data)
+    data_str = input ("Please enter your choice (1-5 and press enter): \n")
+
+display_contact_menu()    
