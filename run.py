3a5f11fec83c2dc33468a94705fbdb9dd8c5bf8b
Caroline Danielsson <caroline.e.danielsson@gmail.com>
2023-06-21 08:44:21 +0000

    Code has been modified to follow the PEP8 style guidelines.

--- run.py
+++ run.py
@@ -8,16 +8,18 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 
-CREDS = Credentials.from_service_account_file('creds.json')  
+CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('contact_book')      
+SHEET = GSPREAD_CLIENT.open('contact_book')
+
 
 def display_contact_menu():
     """
     Displays the Contact Book menu and handles the selected option.
     This function asks the user to choose an option from the menu.
-    If the user wants to add a new contact, it calls the add_contact() function.
+    If the user wants to add a new contact,
+    it calls the add_contact() function.
     """
     while True:
         print("\nWelcome to Contact Book!\n")
@@ -27,12 +29,19 @@ def display_contact_menu():
         print(Fore.CYAN + "4. " + Style.RESET_ALL + "Delete a contact")
         print(Fore.CYAN + "5. " + Style.RESET_ALL + "Exit\n")
 
-        data_str = input (Fore.CYAN + "Please enter your choice (1-5 and press enter): \n" + Style.RESET_ALL)
+        data_str = input(
+            Fore.CYAN +
+            "Please enter your choice (1-5 and press enter): \n"
+            + Style.RESET_ALL
+            )
 
         try:
             value = int(data_str)
             if not (1 <= value <= 5):
-                raise ValueError(Fore.RED + f"\nInvalid value: {value} Please enter a number from 1 to 5.")
+                raise ValueError(
+                    Fore.RED + f"\nInvalid value:" +
+                    "{value} Please enter a number from 1 to 5."
+                    )
         except ValueError as e:
             print(Fore.RED + f"\n{e} Please try again.\n" + Style.RESET_ALL)
             continue
@@ -44,52 +53,72 @@ def display_contact_menu():
         elif value == 3:
             display_all_contacts()
         elif value == 4:
-            delete_contact()    
+            delete_contact()
         elif value == 5:
-            exit_contact_menu()    
-        else: 
-            print("\nPlease try again.")        
-       
+            exit_contact_menu()
+        else:
+            print("\nPlease try again.")
+
 
 def validate_input(prompt, data_type):
     """
-    "This function validates user input based on the specified data type 
+    This function validates user input based on the specified data type
     and returns the validated input value. It takes two arguments:
     prompt (str): The message to display as a prompt.
-    data_type (str): The type of data to validate. Supported values are 'name', 'email', and 'phone'.
-    The function returns a string, which is the validated input value. 
-    If the input value does not match the specified data type, a ValueError is raised."
-    """  
+    data_type (str): The type of data to validate. Supported values are
+    'name', 'email', and 'phone'.
+    The function returns a string, which is the validated input value.
+    If the input value does not match the specified data type,
+    a ValueError is raised."
+    """
     while True:
         value = input(prompt)
 
         if data_type == "name":
             if not value.replace(" ", "").isalpha():
-                print(Fore.RED + "\nInvalid name: Only letters and blank spaces are allowed.\n" + Style.RESET_ALL)
+                print(
+                    Fore.RED +
+                    "\nInvalid name:" +
+                    "Only letters and blank spaces are allowed.\n"
+                    + Style.RESET_ALL
+                    )
                 continue
         elif data_type == "email":
             if "@" not in value:
-                print(Fore.RED + "\nInvalid email: Must contain the @ symbol.\n" + Style.RESET_ALL)
+                print(
+                    Fore.RED +
+                    "\nInvalid email: Must contain the @ symbol.\n" +
+                    Style.RESET_ALL
+                    )
                 continue
         elif data_type == "phone":
             if not value.replace(" ", "").isdigit():
-                print(Fore.RED + "\nInvalid phone: Only numbers are allowed, and no blank spaces.\n" + Style.RESET_ALL)
+                print(
+                    Fore.RED +
+                    "\nInvalid phone:" +
+                    "Only numbers are allowed, and no blank spaces.\n"
+                    + Style.RESET_ALL
+                    )
                 continue
         else:
             print(Fore.RED + "\nInvalid data type.\n" + Style.RESET_ALL)
             continue
 
-        return value                                  
+        return value
+
 
 def add_contact():
     """
-    Allows the user to enter contact details (name, email, and phone), 
-    validates the input, and adds the contact to the Google Sheet if confirmed by the user.
+    Allows the user to enter contact details (name, email, and phone),
+    validates the input, and adds the contact to the Google Sheet
+    if confirmed by the user.
 
-    This function prompts the user to enter contact details and validates each input.
-    If the input is valid, it displays the entered details and asks for confirmation 
-    to add the contact. If the user confirms, the contact is added to the Google Sheet.
-    After adding the contact, the function provides options to return to the start or exit the program.
+    This function prompts the user to enter contact details and
+    validates each input. If the input is valid, it displays the entered
+    details and asks for confirmation to add the contact. If the user confirms,
+    the contact is added to the Google Sheet.
+    After adding the contact, the function provides options to return
+    to the start or exit the program.
     """
     while True:
         name = validate_input("Enter contact name:\n", "name")
@@ -107,30 +136,50 @@ def add_contact():
         print("-------------------------")
 
         while True:
-            choice = input(Fore.CYAN +"\nDo you want to add the contact? ('Y' for yes or 'N' for no):\n" + Style.RESET_ALL)
+            choice = input(
+                Fore.CYAN +
+                "\nDo you want to add the contact?" +
+                "('Y' for yes or 'N' for no):\n"
+                + Style.RESET_ALL
+                )
 
             if choice.upper() == "Y":
                 contact_worksheet.append_row(list(new_contact.values()))
-                print(Fore.GREEN + "\nContact added successfully!\n" + Style.RESET_ALL)
+                print(
+                    Fore.GREEN +
+                    "\nContact added successfully!\n" + Style.RESET_ALL
+                    )
             elif choice.upper() == "N":
-                print("\nThe contact has not been added to the Contact Book.\n") 
+                print(
+                    "\nThe contact has not been added to the Contact Book.\n"
+                    )
             else:
-                print(Fore.RED +"\nInvalid choice. Please try again." + Style.RESET_ALL)
+                print(
+                    Fore.RED +
+                    "\nInvalid choice. Please try again." + Style.RESET_ALL
+                    )
                 continue
-            
+
             exit_choice()
+
 
 def search_contact():
     """
-    Allows the user to search for a contact in the Google Sheet based on the provided name or email,
-    displaying the matching contacts.
-    
-    This function prompts the user to enter a name or email of the contact they want to search for.
-    It searches the contacts in the Google Sheet and creates a list of matching contacts, if any.
-    After displaying the results, it offers the option to search again or return to the start menu.
+    Allows the user to search for a contact in the Google Sheet based
+    on the provided name or email, displaying the matching contacts.
+
+    This function prompts the user to enter a name or email of the contact
+    they want to search for.It searches the contacts in the Google Sheet and
+    creates a list of matching contacts, if any.
+    After displaying the results, it offers the option to search again
+    or return to the start menu.
     """
     while True:
-        search_term = input(Fore.CYAN + "\nEnter name or email of the contact you want to search for:\n" + Style.RESET_ALL)                            
+        search_term = input(
+            Fore.CYAN +
+            "\nEnter name or email of the contact you want to search for:\n"
+            + Style.RESET_ALL
+            )
         contact_worksheet = SHEET.worksheet("contact")
         contacts = contact_worksheet.get_all_records()
 
@@ -140,8 +189,8 @@ def search_contact():
                 if search_term.lower() in contact["Name"].lower() or search_term.lower() in contact["Email"].lower():
                     found_contacts.append(contact)
             except AttributeError:
-                continue    
-        
+                continue
+
         if found_contacts:
             print("\nFound contacts:")
             print("-------------------------")
@@ -162,15 +211,19 @@ def search_contact():
         else:
             print(Fore.RED + "\nInvalid choice. Try again" + Style.RESET_ALL)
 
+
 def display_all_contacts():
     """
     Displays all the contacts stored in the Google Sheet.
 
-    This function gets all the contact records from the Google Sheet and shows them in an organized way.
-    If there are contacts available, it prints the name, email, and phone number of each contact.
-    If no contacts are found, it prints a message saying that there are no contacts.
+    This function gets all the contact records from the Google Sheet
+    and shows them in an organized way. If there are contacts available,
+    it prints the name, email, and phone number of each contact.
+    If no contacts are found, it prints a message saying that
+    there are no contacts.
 
-    After showing the contacts, the function gives options to go back to the start or exit the program.
+    After showing the contacts, the function gives options to go back
+    to the start or exit the program.
     """
     while True:
         contact_worksheet = SHEET.worksheet("contact")
@@ -185,24 +238,32 @@ def display_all_contacts():
                 print(f"Phone: {contact['Phone']}")
                 print("-------------------------")
         else:
-            print("\nNo contacts found.")                
+            print("\nNo contacts found.")
 
         exit_choice()
+
 
 def delete_contact():
     """
     Allows the user to delete a contact from the contact book.
 
-    The function prompts the user to enter the name or email of the contact they want to delete.
-    It searches for contacts that match the search term and displays the matching contacts.
-    The user is then asked to choose the contact they want to delete by entering the corresponding number.
-    The chosen contact is displayed, and the user is asked to confirm the deletion.
+    The function prompts the user to enter the name or email of
+    the contact they want to delete. It searches for contacts that match
+    the search term and displays the matching contacts. The user is then asked
+    to choose the contact they want to delete by entering
+    the corresponding number. The chosen contact is displayed,
+    and the user is asked to confirm the deletion.
     If confirmed, the contact is deleted from the contact book.
 
-    After deleting a contact, the user can choose to search for another contact to delete or return to the main menu.
+    After deleting a contact, the user can choose to search for another contact
+    to delete or return to the main menu.
     """
     while True:
-        search_term = input(Fore.CYAN + "\nEnter the name or email of the contact you want to delete: \n" + Style.RESET_ALL)
+        search_term = input(
+            Fore.CYAN +
+            "\nEnter the name or email of the contact you want to delete: \n" +
+            Style.RESET_ALL
+            )
         contact_worksheet = SHEET.worksheet("contact")
         contacts = contact_worksheet.get_all_records()
 
@@ -212,8 +273,8 @@ def delete_contact():
                 if search_term.lower() in contact["Name"].lower() or search_term.lower() in contact["Email"].lower():
                     found_contacts.append(contact)
             except AttributeError:
-                continue    
-        
+                continue
+
         if found_contacts:
             print("\nFound contacts:")
             print("-------------------------")
@@ -227,18 +288,27 @@ def delete_contact():
             continue
 
         while True:
-                delete_choice = input(Fore.CYAN + "\nEnter the number of the contact you want to delete (or 'C' to cancel):\n" + Style.RESET_ALL)
-                if delete_choice.upper() == "C":
-                    print("\nDeletion canceled. You will return to start")
-                    display_contact_menu()
-                else:
-                    try:
-                        delete_index = int(delete_choice) - 1
-                        if not (0 <= delete_index < len(found_contacts)):
-                            raise ValueError()
+            delete_choice = input(
+                Fore.CYAN +
+                "\nEnter the number of the contact you want to delete" +
+                "(or 'C' to cancel):\n"
+                + Style.RESET_ALL
+                )
+            if delete_choice.upper() == "C":
+                print("\nDeletion canceled. You will return to start")
+                display_contact_menu()
+            else:
+                try:
+                    delete_index = int(delete_choice) - 1
+                    if not (0 <= delete_index < len(found_contacts)):
+                        raise ValueError()
                         break
-                    except ValueError:
-                        print(Fore.RED + "\nInvalid choice. Please try again" + Style.RESET_ALL)
+                except ValueError:
+                    print(
+                        Fore.RED +
+                        "\nInvalid choice. Please try again" +
+                        Style.RESET_ALL
+                        )
 
         contact_to_delete = found_contacts[delete_index]
         print("\nContact details:")
@@ -249,45 +319,71 @@ def delete_contact():
         print("-------------------------")
 
         while True:
-            confirm_choice = input(Fore.CYAN + "\nAre you sure you want to delete this contact? ('Y' for yes or 'N' for no):\n" + Style.RESET_ALL)
+            confirm_choice = input(
+                Fore.CYAN +
+                "\nAre you sure you want to delete this contact?" +
+                "('Y' for yes or 'N' for no):\n" +
+                Style.RESET_ALL
+                )
             if confirm_choice.upper() == "Y":
                 contact_worksheet.delete_rows(delete_index + 2)
-                print(Fore.GREEN + "\nContact deleted successfully!" + Style.RESET_ALL)
+                print(
+                    Fore.GREEN +
+                    "\nContact deleted successfully!" + Style.RESET_ALL
+                    )
             elif confirm_choice.upper() == "N":
                 print("\nContact deletion canceled.")
             else:
-                print(Fore.RED + "\nInvalid choice. Please try again.\n" + Style.RESET_ALL)
+                print(
+                    Fore.RED +
+                    "\nInvalid choice. Please try again.\n" + Style.RESET_ALL
+                    )
                 continue
-            
-            while True:                                
-                exit_choice = input(Fore.CYAN + "\nDo you want to search for another contact to delete? ('Y' for yes or 'N' for no):\n" + Style.RESET_ALL)
+
+            while True:
+                exit_choice = input(
+                    Fore.CYAN +
+                    "\nDo you want to search for another contact to delete?" +
+                    "('Y' for yes or 'N' for no):\n" +
+                    Style.RESET_ALL
+                    )
                 if exit_choice.upper() == "Y":
                     delete_contact()
                 elif exit_choice.upper() == "N":
                     print("\nYou will return to start.")
                     display_contact_menu()
                 else:
-                    print(Fore.RED + "\nInvalid choice. Please try again" + Style.RESET_ALL)
+                    print(
+                        Fore.RED +
+                        "\nInvalid choice. Please try again" + Style.RESET_ALL
+                        )
 
-            
+
 def exit_choice():
     """
     Handles the choice to either return to the start menu or exit the program.
-    
-    This function let the user to choose between returning to the start menu or exiting the program.
-    If the user chooses to return to the start menu, it calls the display_contact_menu() function.
-    If the user chooses to exit the program, it displays a goodbye message and exits.
+
+    This function let the user to choose between returning to the start menu
+    or exiting the program. If the user chooses to return to the start menu,
+    it calls the display_contact_menu() function. If the user chooses to exit
+    the program, it displays a goodbye message and exits.
     """
     while True:
-        choice = input(Fore.CYAN + "\nPress 'S' to go to start or 'E' to exit:\n" + Style.RESET_ALL)
+        choice = input(
+            Fore.CYAN +
+            "\nPress 'S' to go to start or 'E' to exit:\n" + Style.RESET_ALL
+            )
 
         if choice.upper() == "S":
             print("\nStarting from the beginning.\n")
-            display_contact_menu() 
+            display_contact_menu()
         elif choice.upper() == "E":
             exit_contact_menu()
         else:
-            print(Fore.RED + "\nInvalid choice. Please try again." + Style.RESET_ALL)
+            print(
+                Fore.RED +
+                "\nInvalid choice. Please try again." + Style.RESET_ALL
+                )
 
 
 def exit_contact_menu():
@@ -297,5 +393,5 @@ def exit_contact_menu():
     print("\nThank you for using Contact Book. Have a great day!\n")
     exit()
 
-        
+
 display_contact_menu()
